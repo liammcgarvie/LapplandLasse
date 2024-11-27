6683ba05bcffcc8323730f19beb4f6c923a428e7
EliasBaten <e24elihe@student.his.se>
2024-11-27 16:24:58 +0100

    Update FollowPlayer.cs

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
+
+    [SerializeField] private float z;
     
     void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y, z);
     }
 }
