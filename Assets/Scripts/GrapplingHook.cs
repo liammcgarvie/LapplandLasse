07d694cbabf3e47f4766325c32cfaabcb47e4e5f
EliasBaten <e24elihe@student.his.se>
2024-11-27 13:20:44 +0100

    Elias Scene (movement + grappling hook implementation)

---
+++ Assets/Scripts/GrapplingHook.cs
@@ -0,0 +1,89 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GrapplingHook : MonoBehaviour
+{
+    [SerializeField] private LayerMask grappleLayer;
+    [SerializeField] private LineRenderer rope;
+    [SerializeField] private Camera cam;
+    
+    private Vector3 grapplePoint;
+    private DistanceJoint2D joint;
+    private Vector2 direction;
+    
+    public UnityEvent OnGrapple;
+    public UnityEvent OffGrapple;
+    
+    void Start()
+    {
+        // Sets joint to the local DistanceJoint2D and disables the grappling hook at the start
+        joint = gameObject.GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+        rope.enabled = false;
+    }
+    
+    void Update()
+    {
+        // Convert mouse position to world space
+        Vector3 mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            cam.nearClipPlane // Use the near clipping plane distance
+        ));
+        
+        // Calculates the direction from the player to the mouse
+        direction = (mouseWorldPos - transform.position).normalized;
+
+        // Starts the Grapple
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Performs a raycast in the calculated direction
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, grappleLayer);
+
+            // Debugs the ray to visualize it in the Scene view
+            Debug.DrawRay(transform.position, direction * 10, Color.red, 2f);
+
+            // Starts grappling if the raycast hits a viable layer and if the direction is not downwards
+            if (hit.collider && direction.y >= 0)
+            {
+                OnGrapple.Invoke();
+                grapplePoint = hit.point;
+                joint.connectedAnchor = grapplePoint;
+                joint.distance = Vector2.Distance(transform.position, grapplePoint);
+                joint.enabled = true;
+
+                rope.enabled = true;
+                rope.SetPosition(0, grapplePoint);  // Anchors end point
+                rope.SetPosition(1, transform.position); // Players position
+            }
+        }
+
+        // Ends the Grapple
+        if (Input.GetMouseButtonUp(0))
+        {
+            OffGrapple.Invoke();
+            joint.enabled = false;
+            rope.enabled = false;
+        }
+
+        // Updates the ropes player end position
+        if (rope.enabled)
+        {
+            rope.SetPosition(1, transform.position);
+        }
+
+        // Disables the rope if the joint breaks
+        if (joint.enabled == false)
+        {
+            rope.enabled = false;
+        }
+
+        // Disables the joint if the direction is downwards
+        if (direction.y < 0)
+        {
+            joint.enabled = false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/MyMovement.cs
@@ -0,0 +1,121 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MyMovement : MonoBehaviour
+{
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float maxSpeed = 10f;
+    [SerializeField] private float jumpForce = 10f;
+    
+    public bool controlEnabled { get; set; }
+
+    private Vector2 moveInput;
+    private Vector2 startPosition;
+    private bool isGrounded;
+    private bool isMoving;
+    private bool isGrappling;
+    private bool justGrappled;
+
+    private Rigidbody2D rb;
+    private CircleCollider2D groundCheckCollider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        groundCheckCollider = GetComponent<CircleCollider2D>();
+        groundCheckCollider.isTrigger = true;
+        
+        controlEnabled = true;
+        
+        startPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 currentVelocity = rb.velocity;
+
+        if (isMoving || (isGrappling == false && justGrappled == false))
+        {
+            rb.velocity = new Vector2(TranslateInputToVelocityX(moveInput), currentVelocity.y);
+        }
+        
+        isGrounded = IsGrounded();
+    }
+
+    private void Update()
+    {
+        // If you are grappling justGrappled is set to true
+        if (isGrappling)
+        {
+            justGrappled = true;
+        }
+
+        // Sets justGrappled to false when you have landed on the ground after grappling
+        if (isGrounded && isGrappling == false)
+        {
+            justGrappled = false;
+        }
+    }
+    
+    private bool IsGrounded() // Checks if the player is on the ground
+    {
+        if (groundCheckCollider.IsTouchingLayers(groundLayer))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    float TranslateInputToVelocityX(Vector2 input)
+    {
+        return input.x * maxSpeed;
+    }
+    
+    public void PositionReset()
+    {
+        transform.position = startPosition;
+    }
+
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            moveInput = context.ReadValue<Vector2>().normalized;
+            isMoving = true;
+        }
+        else if (context.canceled)
+        {
+            moveInput = Vector2.zero;
+            isMoving = false;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Jump();
+        }
+    }
+    
+    public void StartGrapple() // Can be called with events
+    {
+        isGrappling = true;
+    }
+
+    public void EndGrapple() // Can be called with events
+    {
+        isGrappling = false;
+    }
+}
+
