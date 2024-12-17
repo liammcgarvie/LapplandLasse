0632cb58bfc02ecff23159233857644ce3ff3a68
EliasBaten <e24elihe@student.his.se>
2024-12-17 14:04:37 +0100

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Enemy : MonoBehaviour
 {
-    
+    public UnityEvent onJumped;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onJumped.Invoke();
+        }
+    }
 }
