b7fda09ba12f9bb18a558402be2acd437fd95143
EliasBaten <e24elihe@student.his.se>
2024-12-17 15:31:53 +0100

    Nu kan man hoppa på fiender

--- Assets/Scripts/EnemyBouncePad.cs
+++ Assets/Scripts/EnemyBouncePad.cs
@@ -3,13 +3,25 @@ using UnityEngine.Events;
 
 public class EnemyBouncePad : MonoBehaviour
 {
+    [Tooltip("This is the layer where a player will be able to jump")]
+    [SerializeField] private LayerMask groundLayer;
+    [Tooltip("This is the collider that is on the player which checks if the player is on the ground")]
+    [SerializeField] private CircleCollider2D groundCheckCollider;
+    
     private int i;
+    
+    private bool isGrounded;
     
     public UnityEvent onJumped;
 
     private void Start()
     {
         i = 0;
+    }
+
+    private void Update()
+    {
+        isGrounded = IsGrounded();
     }
     
     private void OnTriggerEnter2D(Collider2D other)
@@ -18,9 +30,22 @@ public class EnemyBouncePad : MonoBehaviour
         {
             if (i == 0)
             {
-                i++;
-                onJumped.Invoke();
+                if (isGrounded == false)
+                {
+                    i++;
+                    onJumped.Invoke();
+                }
             }
         }
+    }
+    
+    private bool IsGrounded() // Checks if the player is on the ground
+    {
+        if (groundCheckCollider.IsTouchingLayers(groundLayer))
+        {
+            return true;
+        }
+        
+        return false;
     }
 }
