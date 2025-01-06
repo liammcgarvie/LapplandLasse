dc6fb96b9f091b5fcf5e4b61319a35568b9bb299
EliasBaten <e24elihe@student.his.se>
2025-01-06 14:41:22 +0100

    marker update

--- Assets/Scripts/GrappleMarker.cs
+++ Assets/Scripts/GrappleMarker.cs
@@ -1,16 +1,40 @@
-using System.Linq;
 using UnityEngine;
 
 public class GrappleMarker : MonoBehaviour
 {
     [Tooltip("This is the camera which will be used to cast the rope object (the main camera most likely)")]
     [SerializeField] private Camera cam;
+    [SerializeField] private float radius;
+    
+    private Vector2 closestPoint;
+    
+    private string[] layers;
 
+    private void Awake()
+    {
+        layers = new string[2];
+
+        layers[0] = "Ground";
+        layers[1] = "Enemy";
+    }
+    
     private void Update()
     {
         // Convert mouse position to world space
         Vector3 mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
         
-        transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, transform.position.z);
+        RaycastHit2D hit = Physics2D.CircleCast(new Vector2(mouseWorldPos.x, mouseWorldPos.y), radius, Vector2.zero, 0, LayerMask.GetMask(layers));
+
+        if (hit.collider)
+        {
+            closestPoint = Physics2D.ClosestPoint(new Vector2(mouseWorldPos.x, mouseWorldPos.y), hit.collider);
+                    
+            transform.position = closestPoint;
+        }
+        else if (!hit.collider)
+        {
+            transform.position = mouseWorldPos;
+        }
+        
     }
 }
