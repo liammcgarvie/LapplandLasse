532ffbf3f1f2bb19475d9c3ef58de8a1048dc323
EliasBaten <e24elihe@student.his.se>
2025-01-06 13:26:17 +0100

    Update GrappleMarker.cs

--- Assets/Scripts/GrappleMarker.cs
+++ Assets/Scripts/GrappleMarker.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class GrappleMarker : MonoBehaviour
 {
-    
+    [Tooltip("This is the camera which will be used to cast the rope object (the main camera most likely)")]
+    [SerializeField] private Camera cam;
+
+    private void Update()
+    {
+        // Convert mouse position to world space
+        Vector3 mouseWorldPos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane));
+    }
 }
